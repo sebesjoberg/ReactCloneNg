c7b9d58ecffa5c211262820939839a418fd4fa29
sebesjoberg <ss69109@gmail.com>
2022-11-22 20:22:49 +0100

    made component!

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import { React, Component } from 'react';
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React2
-          </a>
-        </header> 
-      </div>
-    );
-  }
-  
-}
+import React, { Component } from 'react'
 
-export default App;
+export default class App extends Component {
+    state = {  } 
+    render() { 
+        return <h1>Hello World</h1>;
+    }
+}
