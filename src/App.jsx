a90570790881a2d6cd289b7ee31c6adadc64a258
sebesjoberg <ss69109@gmail.com>
2022-11-24 09:29:10 +0100

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import EventPage from "./pages/EventPage";
+
 export default class App extends Component {
   state = {};
   render() {
-    return <EventPage />;
+    const router = createBrowserRouter([
+      {
+        path: "/",
+        element: <EventPage />,
+        errorElement: <EventPage />,
+      },
+    ]);
+    return <RouterProvider router={router} />;
   }
 }
